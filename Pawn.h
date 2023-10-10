fce25a4f2c6d48e079a3660ef25d1da996c24909
Kian Slagter <kianslagter@gmail.com>
2023-10-10 14:15:29 +1030

    pawn

--- Pawn.h
+++ Pawn.h
@@ -4,11 +4,25 @@
 using namespace std;
 #include "Piece.h"
 
-class Pawn : public Piece
-{
-private:
-public:
-    string getName() override { return "Pawn"; }
-    Pawn(/* args */){};
-    ~Pawn() override {};
+class Pawn : public Piece {
+ private:
+  int pieceValue = 1;
+  string type = "Pawn";
+  bool promotionCheck;
+  string Colour;
+
+ public:
+  string getName() override { return type; }
+
+  int getPieceValue() { return pieceValue; }
+
+  bool promotionCheck() {
+    if (Colour == "white") return 1;
+  }
+  else {
+    return 0;
+  }
+
+  Pawn(/* args */){};
+  ~Pawn() override{};
 };
